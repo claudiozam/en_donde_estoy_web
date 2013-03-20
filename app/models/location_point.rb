2d727120176ec467fcb147644aadc084c54153d2
sebastianGitDL <sebastian@xagax.com>
2013-03-20 14:16:32 -0700

    fixed relation between location point and locations

--- app/models/location_point.rb
+++ app/models/location_point.rb
@@ -1,13 +1,13 @@
 class LocationPoint < ActiveRecord::Base
-  	geocoded_by :address
+  geocoded_by :address
 	
+	belongs_to :locations 
+
 	attr_accessible :device_id, :latitude, :longitude
-  	validates :latitude, :presence => true, :numericality => true
+	validates :latitude, :presence => true, :numericality => true
 	validates :longitude, :presence => true, :numericality => true
 
 	def get_address
 		results = Geocoder.search([latitude, longitude].join(','))
 	end
-	
-
 end
